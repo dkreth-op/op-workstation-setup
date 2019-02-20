34362bfc410a8817f380e11dc1c48fdb1a885643
dkreth-op <47815552+dkreth-op@users.noreply.github.com>
2019-02-20 14:34:18 +0100

    1.0
    Added OP-Workstation-Setup

---
+++ scripts/opWorkstation.ps1
@@ -0,0 +1,27 @@
+#--- Browsers ---
+choco install -y googlechrome
+
+#--- BasicApps ---
+choco install -y adobereader
+choco install -y lenovo-thinkvantage-system-update
+choco install -y notepad2
+choco install -y ultravnc
+choco install -y winscp
+choco install -y vlc
+choco install -y office365business
+choco install -y 7zip
+choco install -y slack
+choco install -y keepass
+
+
+#--- DevApps ---
+choco install -y git
+choco install -y vscode
+choco install -y adoptopenjdk8
+choco install -y intellijidea-community
+choco install -y gradle
+choco install -y postman
+choco install -y awscli
+choco install -y nodist
+choco install -y docker-desktop
+
