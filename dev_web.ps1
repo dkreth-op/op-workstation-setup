47bb9b4374fb998bb9efdcd00dcebbb6857140d0
dkreth-op <47815552+dkreth-op@users.noreply.github.com>
2019-02-20 14:42:50 +0100

    Update dev_web.ps1

--- dev_web.ps1
+++ dev_web.ps1
@@ -27,23 +27,6 @@ executeScript "FileExplorerSettings.ps1";
 executeScript "RemoveDefaultApps.ps1";
 executeScript "opWorkstation.ps1";
 
-#--- Tools ---
-#--- Installing VS and VS Code with Git
-# See this for install args: https://chocolatey.org/packages/VisualStudio2017Community
-# https://docs.microsoft.com/en-us/visualstudio/install/workload-component-id-vs-community
-# https://docs.microsoft.com/en-us/visualstudio/install/use-command-line-parameters-to-install-visual-studio#list-of-workload-ids-and-component-ids
-# visualstudio2017community
-# visualstudio2017professional
-# visualstudio2017enterprise
-
-
-#Update-SessionEnvironment #refreshing env due to Git install
-
-#--- UWP Workload and installing Windows Template Studio ---
-
-executeScript "WindowsTemplateStudio.ps1";
-executeScript "GetUwpSamplesOffGithub.ps1";
-
 #--- reenabling critial items ---
 Enable-UAC
 Enable-MicrosoftUpdate
